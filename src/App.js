f7724df231074ec222d95196d871ffa494c45c55
mmosan <mosan@fi.uba.ar>
2023-12-21 23:00:49 -0300

    set up of the react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+import { Helmet } from 'react-helmet';
+import './styles/styles.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Grid from './components/Grid';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <title>Portfolio de Martín "canillas" Osan</title>
+      </Helmet>
+      <Header />  
+      <Main />
+      <Grid />   
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '.././styles/header.css'
+
+export default function Header() {
+    return (
+        <div className='headerContainer'>
+            <h1>Martín Osan</h1>
+            <h2>Developer</h2>   
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus interdum placerat aliquet. Nunc quis metus non lorem sollicitudin vestibulum. Morbi tempor et odio vitae rhoncus. Donec sodales aliquet vestibulum. Vestibulum elit leo, lobortis quis rutrum nec, iaculis nec sapien. Vestibulum a lacus ut magna volutpat iaculis. Praesent interdum ut metus vel aliquam. Vivamus ultrices interdum erat non dignissim. Praesent interdum sodales ornare. Aenean dignissim est in mauris venenatis pretium. Ut venenatis erat eget pellentesque accumsan. Sed ac massa massa.</p>
+        </div>
+    );
+}

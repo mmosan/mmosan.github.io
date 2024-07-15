fff648ade57fb7099ef28a695334a900736c8292
mmosan <mosan@fi.uba.ar>
2024-07-15 17:58:08 -0300

    first build

--- src/App.js
+++ src/App.js
@@ -4,16 +4,32 @@ import './styles/styles.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Grid from './components/Grid';
+import Sschess from './components/Sschess';
+import BarraLinks from './components/BarraLinks';
+import LineaVertical from './components/LineaVertical'
+import React from 'react';
+import ParrafoPersonal from './components/ParrafoPersonal';
+import GraficosEstadistica from './components/GraficosEstadistica';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <title>Portfolio de Martín "canillas" Osan</title>
-      </Helmet>
-      <Header />  
-      <Main />
-      <Grid />   
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <Helmet>
+          <title>Portfolio de Martín "canillas" Osan</title>
+        </Helmet>
+        <BarraLinks />  
+        <Header />  
+        <div className='cuerpo'>  
+          <div className="wrapper">
+              <Sschess />
+              <Main />
+          </div> 
+        <ParrafoPersonal />
+        <GraficosEstadistica />
+        </div>   
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,21 @@ import '.././styles/styleMain.css'
 export default function Main() {
     return(
         <div className='mainContainer'>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus interdum placerat aliquet. Nunc quis metus non lorem sollicitudin vestibulum. Morbi tempor et odio vitae rhoncus. Donec sodales aliquet vestibulum. Vestibulum elit leo, lobortis quis rutrum nec, iaculis nec sapien. Vestibulum a lacus ut magna volutpat iaculis. Praesent interdum ut metus vel aliquam. Vivamus ultrices interdum erat non dignissim. Praesent interdum sodales ornare. Aenean dignissim est in mauris venenatis pretium. Ut venenatis erat eget pellentesque accumsan. Sed ac massa massa.</p>
+           <div className='parrafoInstruccionesContainer'>
+                <div className='parrafoInstrucciones'>
+                  <h4> Como se juega </h4>
+                  <p> El juego consta de una pieza roja solo se puede mover a casillas ocupadas por otras piezas siguiendo las reglas convencionales del ajedrez.
+            Cuando come una pieza se transforma en ella. <br></br>El objetivo es comer todas las piezas del tablero.<br></br> Nota: Se puede saltar por encima de otras piezas como lo suele hacer el caballo 
+                  </p>
+                </div>
+              </div> <div className='parrafoContainer'>
+                <div className='parrafoSSchess'>
+                    <h4>Un poco sobre el proyecto...</h4>
+                    <p> Este es un juego que desarrollé en Python para una de las primeras materias que curse en la facultad. Me propuse rehacerlo en Javascript para lograr un mayor entendimiento de dicho lenguaje.<br></br>
+            Es un proyecto que me gustó mucho revisitar ya que soy un aficionado del ajedrez (pueden ver mi cuenta en <a href="https://lichess.org/@/BadDaysXd" target="_blank">LICHESS</a>) y, al a analizar mi propio código para rehacerlo, logré cuantificar cuanto crecí como desarrollador en el transcurso de estos años.
+                    </p> 
+                </div>
+            </div>
         </div>
     );
 }
